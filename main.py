297012dd49760ac98c486b39521390f59a7a43fe
Ninazu00 <ba.mohammad2005@gmail.com>
2025-12-07 21:20:33 +0200

    Added time calculation for cultural algorithm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import culturalAlgorithm
+import time
 
 #Backtracking testing
 #Fine tuning variables for the cultural algorithm
@@ -11,6 +12,7 @@ totalItems = culturalAlgorithm.initializeTotalItems(3,7,20)
 binSize = 10
 binAmount = 0
 bestBin = culturalAlgorithm.Individual()
+startTimeCA = time.time()
 #Executes the cultural algorithm repetitively until all items are packed into bins.
 while totalItems:
     for itemID in bestBin.items.keys():
@@ -19,6 +21,9 @@ while totalItems:
         break
     bestBin = culturalAlgorithm.generateBinCulturalAlgorithm(maxGenerations, populationSize, mutationRate, totalItems, binSize)
     binAmount += 1
-    print("Items in best bin:", bestBin.items.values())
-    print("Best bin fill rate:", bestBin.getFillRate(binSize))
+    print("Items in bin:", bestBin.items.values())
+    print("Fill rate:", bestBin.getFillRate(binSize))
+#Calculates the time it took the cultural algorithm to run
+elapsedTimeCA = time.time()- startTimeCA
 print("Total number of bins used: ", binAmount)
+print("Time elapsed: ", elapsedTimeCA)
