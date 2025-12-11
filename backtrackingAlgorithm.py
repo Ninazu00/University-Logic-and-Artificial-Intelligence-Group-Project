2c0cca49d8674b12731cf0f8af9bee75c3dca27f
daily1mostafa <daily1mostafa@gmail.com>
2025-12-11 20:49:39 +0200

    Created utils.py and moved the backtracking helper functions there (I also changed the helper functions callings in backtracking to match its new location).

--- backtrackingAlgorithm.py
+++ backtrackingAlgorithm.py
@@ -1,100 +1,7 @@
 import math
 import copy
 import time
-
-
-def sortItems(itemsList):
-    """
-    This function sorts the items in a descending order.
-
-    Arguments: list[float]: List of item sizes
-
-    Returns: list[float]: New list of items sorted in a descending order based on size.
-    """
-    return sorted(itemsList, reverse=True)
-
-def placeItem(item, binIndex, usedBins, binRemainingCapacities):
-    """
-    This function places an item in a specific bin and updates its remaining capacity.
-
-    Arguments:
-        item (float): Size of the item to place.
-        binIndex (int): Index of the target bin in usedBins.
-        usedBins (list[list[float]]): Current items in each bin
-        binRemainingCapacities (list[float]): Remaining capacity for each bin.
-
-    Returns: None.
-    """
-    usedBins[binIndex].append(item)
-    binRemainingCapacities[binIndex] -= item
-
-def removeItem(item, binIndex, usedBins, binRemainingCapacities):
-    """
-    This function removes the last item from a specific bin and restores its remaining capacity.
-
-    Arguments:
-        item (float): Size of the item to remove.
-        binIndex (int): Index of the bin in usedBins.
-        usedBins (list[list[float]]): Current items in each bin.
-        binRemainingCapacities (list[float]): Remaining capacity for each bin.
-
-    Returns: None
-    """
-    removed = usedBins[binIndex].pop()
-    binRemainingCapacities[binIndex] += removed
-
-
-def calculateLowerBound(remainingItems, binCapacity):
-    totalSize = sum(remainingItems)
-    return math.ceil(totalSize / binCapacity)
-
-
-
-def copyBins(bins):
-    return copy.deepcopy(bins)
-  
-
-
-def pruneBranch(usedBins, bestSolution, remainingItems, binCapacity):
-    print("Remaining items when calculating pruneBranch: ", remainingItems)
-    lowerBound = calculateLowerBound(remainingItems, binCapacity)
-    print("Lower bound : ", lowerBound)
-    if len(usedBins) + lowerBound >= len(bestSolution):
-        return True
-
-    return False
-
-
-def initializeSolution(items, binCapacity):
-    bins = []
-    for item in items:
-        placed = False
-        for b in bins:
-            if sum(b) + item <= binCapacity:
-                b.append(item)
-                placed = True
-                break
-        if not placed:
-            bins.append([item])
-    return bins
-
-    pass
-
-def findFeasibleBins(item, binRemainingCapacities):
-    """
-    This function loops through the remaining space in binRemainingCapacities list to find the indices of bins where the given item still fits.
-
-    Arguments: 
-        item (float): Size of the item to place.
-        binRemainingCapacities (list[float]): Remaining capacity for each bin.
-
-    Returns: list[int]: Indices of bins where the item fits.
-    """
-    indices = []
-    for i, remaining in enumerate(binRemainingCapacities):
-        if item <= remaining:
-            indices.append(i)
-    return indices
+import utils
 
 def backtrack(currentIndex, usedBins, binRemainingCapacities, binCapacity, bestSolution, sortedItemsList):
     """
@@ -119,23 +26,23 @@ def backtrack(currentIndex, usedBins, binRemainingCapacities, binCapacity, bestS
     if currentIndex == len(sortedItemsList):
         if len(usedBins) < len(bestSolution):
             bestSolution.clear()
-            bestSolution.extend(copyBins(usedBins))
+            bestSolution.extend(utils.copyBins(usedBins))
         return
 
     item = sortedItemsList[currentIndex]
 
     # Remaining items (including current item) for lower bound pruning
     remainingItems = sortedItemsList[currentIndex:]
-    if pruneBranch(usedBins, bestSolution, remainingItems, binCapacity):
+    if utils.pruneBranch(usedBins, bestSolution, remainingItems, binCapacity):
         return
 
     # Try placing in existing bins (all feasible choices)
-    feasibleBins = findFeasibleBins(item, binRemainingCapacities)
+    feasibleBins = utils.findFeasibleBins(item, binRemainingCapacities)
     for i in feasibleBins:
-        placeItem(item, i, usedBins, binRemainingCapacities)
+        utils.placeItem(item, i, usedBins, binRemainingCapacities)
         backtrack(currentIndex + 1, usedBins, binRemainingCapacities,
                   binCapacity, bestSolution, sortedItemsList)
-        removeItem(item, i, usedBins, binRemainingCapacities)
+        utils.removeItem(item, i, usedBins, binRemainingCapacities)
 
     # Try placing in a new bin
     usedBins.append([item])
@@ -147,8 +54,8 @@ def backtrack(currentIndex, usedBins, binRemainingCapacities, binCapacity, bestS
 
 def solveBinPacking(items, binCapacity):
     print("Items to solve with: ", items)
-    sortedItems = sortItems(items)
-    bestSolution = initializeSolution(sortedItems, binCapacity)
+    sortedItems = utils.sortItems(items)
+    bestSolution = utils.initializeSolution(sortedItems, binCapacity)
     usedBins = []
     binRemainingCapacities = []
     start = time.time()
